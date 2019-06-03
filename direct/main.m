7ffce86abd0613e4a4ce2e0e279d27a21e684513
zach <zach@windows.com>
2019-06-03 11:57:07 -0700

    Reduce discretization steps
    500 steps was taking way too long.
    50 seems to take ~10 minutes and works well enough for now

--- direct/main.m
+++ direct/main.m
@@ -2,7 +2,6 @@
 clear; clc; close all;
 
 % global variables
-global N; N = 500; % Choose here the number of discretization points
 global mu; mu = 3.9915e14;
 global r_E; r_E = 6378145;
 global w_E; w_E = 7.2921159e-5; % rad/s
@@ -15,14 +14,16 @@ global r_min; r_min = r_E+400e3;
 global r_des;
 global x0
 global T;
+global N; 
 
 % Scenario
 T = 3600;
+N = 50; % number of discretization points
 
 r0 = [r_E+408e3;0;0];     % orbit at iss altitude
 v0 = [0;sqrt(mu/r0(1));0];  % circular polar orbit
 x0 = [r0;v0];
-r_des0 = [0;r_E+408e3;0];
+r_des0 = [0;6712e3;1000e3];
 r_des = R3(w_E*T)*r_des0;
 
 % Guess Trajectory
@@ -33,6 +34,6 @@ v_max = 12e3;
 lb = [zeros(3*N,1); -v_max*ones(3*N,1);-u_max*ones(3*N,1)];
 ub = [(1.5*r_E)*ones(3*N,1); v_max*ones(3*N,1); u_max*ones(3*N,1)];
 
-options=optimoptions('fmincon','Display','iter','Algorithm','sqp','MaxFunctionEvaluations',100000,'MaxIterations',100000);
+options=optimoptions('fmincon','Display','iter','Algorithm','sqp','MaxFunctionEvaluations',10000,'MaxIterations',10000);
 [var,Fval,convergence] = fmincon(@cost,var_init,[],[],[],[],lb,ub,@fmin_constraints,options); % Solving the problem
 convergence % = 1, good
