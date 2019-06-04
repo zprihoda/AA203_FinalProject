c2ffa43f64877bc811d40d934695aa88f293081e
zach <zach@windows.com>
2019-06-04 10:06:06 -0700

    Use orbit propogation instead of dynamics

--- direct/fmin_constraints.m
+++ direct/fmin_constraints.m
@@ -6,6 +6,7 @@ function [c,ceq] = fmin_constraints(var)
     global u_max;
     global r_min;
     global x0;
+    global mu;
     
     % load variables
     r1 = var(1:N); r2 = var(N+1:2*N); r3 = var(2*N+1:3*N); 
@@ -17,27 +18,20 @@ function [c,ceq] = fmin_constraints(var)
         r_min - vecnorm([r1,r2,r3]')'];
     
     % Computing dynamical constraints via the Hermite-Simpson rule
-    h = 1.0*T/(1.0*N);
+    dt = 1.0*T/(1.0*N);
     ceq = [];
     for i = 1:N-1
         % Evaluate 
-        x_i = [r1(i);r2(i);r3(i);v1(i);v2(i);v3(i)];
-        u_i = [u1(i);u2(i);u3(i)];
-        xdot_i = fDyn(x_i,u_i);
+        ri = [r1(i);r2(i);r3(i)];
+        vi = [v1(i);v2(i);v3(i)] + [u1(i);u2(i);u3(i)];
         
+        % propogate orbit
+        [rf,vf] = twobody1(mu/1000^3,dt,ri/1000,vi/1000);
+        x_prop = [rf*1000,vf*1000]'; 
+        
+        % constraint
         x_ii = [r1(i+1);r2(i+1);r3(i+1);v1(i+1);v2(i+1);v3(i+1)];
-        u_ii = [u1(i+1);u2(i+1);u3(i+1)];
-        xdot_ii = fDyn(x_ii,u_ii);
-
-        % obtain colocation points
-        x_c = (x_i+x_ii)/2 + h/8*(xdot_i-xdot_ii);
-        u_c = (u_i+u_ii)/2;
-
-        % evaluate colocation point dynamics
-        xdot_c = fDyn(x_c,u_c);
-
-        % constraints
-        ceq(6*(i-1)+1:6*i) = x_ii - x_i - h/6*(xdot_i + 4*xdot_c + xdot_ii);
+        ceq(6*(i-1)+1:6*i) = x_ii - x_prop;
     end
 
     % initial and final conditions
--- direct/main.m
+++ direct/main.m
@@ -11,14 +11,15 @@ global g3; g3 = 1;
 global u_max; u_max = 1;
 global r_min; r_min = r_E+400e3;
 
+global tbcoef; tbcoef = 1;
+
 global r_des;
 global x0
 global T;
 global N; 
 
 % Scenario
-T = 3600;
-N = 50; % number of discretization points
+N = 10; % number of discretization points
 
 r0 = [r_E+408e3;0;0];     % orbit at iss altitude
 v0 = [0;sqrt(mu/r0(1));0];  % circular polar orbit
@@ -26,12 +27,17 @@ x0 = [r0;v0];
 r_des0 = [0;6712e3;1000e3];
 r_des = R3(w_E*T)*r_des0;
 
+% stanford coordinates
+% des_gc = deg2rad([37,-122]);
+% r_des0 = gc2ECEF(des_gc(1),des_gc(2))*(r_E+408e3);
+% r_des = rotz(w_E*T/pi*180)*r_des0;
+
 % Guess Trajectory
 var_init = guessTraj(r0,r_des);
 
 % define search bounds
 v_max = 12e3;
-lb = [zeros(3*N,1); -v_max*ones(3*N,1);-u_max*ones(3*N,1)];
+lb = [-(1.5*r_E)*ones(3*N,1); -v_max*ones(3*N,1);-u_max*ones(3*N,1)];
 ub = [(1.5*r_E)*ones(3*N,1); v_max*ones(3*N,1); u_max*ones(3*N,1)];
 
 options=optimoptions('fmincon','Display','iter','Algorithm','sqp','MaxFunctionEvaluations',10000,'MaxIterations',10000);
